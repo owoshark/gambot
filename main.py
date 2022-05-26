753b6709671f9b8e798280fc71083c561c3b03fa
patrick an <you@example.com>
2022-05-25 22:14:06 -0700

    exception handling and args

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import commands.gambit as gambit
-import discord
-import os
+import discord, os
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
 bot.remove_command('help')
 
-
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='for !help'))
 
+@bot.command()
+async def gp(ctx, tokens=None):
+    try:
+        if not tokens:
+            await ctx.send("Proper usage: !gp tokens")
+        else:
+            await ctx.send(embed=gambit.profit(tokens))
+    except:
+        await ctx.send("An error occurred. Please contact the developer.")
 
 @bot.command()
-async def gp(ctx, tokens):
-    if not tokens:
-        await ctx.send("Proper usage: !gp tokens")
-    else:
-        await ctx.send(embed=gambit.profit(tokens))
-
-
-@bot.command()
-async def help(ctx, *args):
+async def help(ctx):
     await ctx.send(embed=discord.Embed(title='Gambot Commands', description='!gp tokens'))
 
 bot.run(os.environ.get('BOT_TOKEN'))

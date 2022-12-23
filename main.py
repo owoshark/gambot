dbf0b6738ec455db48ce09e855bf28dedd7501a4
patrick an <you@example.com>
2022-12-23 13:49:15 -0800

    revert

--- main.py
+++ main.py
@@ -5,17 +5,6 @@ import discord, os, asyncio
 from datetime import time, datetime
 from json.decoder import JSONDecodeError
 from discord.ext import commands, tasks
-import SimpleHTTPServer
-import SocketServer
-
-PORT = 3000
-
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-
-print("serving at port", PORT)
-httpd.serve_forever()
 
 intents = discord.Intents.default()
 intents.message_content = True

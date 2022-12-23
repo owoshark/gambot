eca04fd3ad2477184e79f10df8958c408e5ffa3b
patrick an <you@example.com>
2022-12-22 17:07:48 -0800

    listen on port

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import discord, os, asyncio
 from datetime import time, datetime
 from json.decoder import JSONDecodeError
 from discord.ext import commands, tasks
+from http.server import HTTPServer
+
+# Get port number from the PORT environment variable or 3000 if not specified
+port = os.getenv('PORT', 3000)
+
+server = HTTPServer(('0.0.0.0', port), MyServer)
+server.serve_forever()
 
 intents = discord.Intents.default()
 intents.message_content = True
